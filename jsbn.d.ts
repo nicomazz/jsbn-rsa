2de2b94e8bd923b0e951e289f5b9173daa175201
Nicolò Mazzucato <nicomazz@google.com>
2019-09-23 17:36:05 +0100

    Added ts types

---
+++ jsbn.d.ts
@@ -0,0 +1,33 @@
+
+declare namespace jsbn {
+  class BigInteger {
+    bitLength(): number;
+    clone(): BigInteger;
+    divide(n: BigInteger): BigInteger;
+    intValue(): number;
+    mod(a: BigInteger): BigInteger;
+    modInverse(m: BigInteger): BigInteger;
+    multiply(n: BigInteger): BigInteger;
+    pow(e: number): BigInteger;
+    shiftLeft(n: number): BigInteger;
+    subtract(n: BigInteger): BigInteger;
+    toByteArray(): number[];
+    toString(b: number): string;
+
+    constructor(a: ArrayLike<number>);
+  }
+}
+
+declare class RSAKey {
+  n: jsbn.BigInteger;
+  e: number;
+
+  setPublic(n: string, e: string): void;
+  setPrivateEx(
+      n: string, e: string, d: string, p: string, q: string, dp: string,
+      dq: string, c: string): void;
+  doPrivate(x: jsbn.BigInteger): jsbn.BigInteger;
+}
+      
+declare const BigInteger: typeof jsbn.BigInteger;
+
